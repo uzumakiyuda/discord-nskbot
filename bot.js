483f77ff17dd699b6288c2da1cc9242e87a9f4a3
uzumakiyuda <63958291+uzumakiyuda@users.noreply.github.com>
2020-04-20 08:29:22 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,10 +18,4 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-    }
-});
-
 client.login(process.env.BOT_TOKEN); 
