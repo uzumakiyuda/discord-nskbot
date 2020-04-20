515d3f2f91afea91d5633e4bd57a156df0dbb0b2
uzumakiyuda <63958291+uzumakiyuda@users.noreply.github.com>
2020-04-20 08:10:11 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const activities_list = [
-    "with the &help command.", 
-    "with the developers console",
-    "with some code", 
-    "with JavaScript"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -14,12 +8,6 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
-        
-client.on('ready', () => {
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
 });
 
 // THIS  MUST  BE  THIS  WAY
