69a432a8c766a424ea8e32c90ea194f1bd75f108
uzumakiyuda <63958291+uzumakiyuda@users.noreply.github.com>
2020-04-20 08:32:28 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const actvs = [
-    "with the &help command.",
-    "with the developers console",
-    "with some code",
-    "with JavaScript"
-];
-
-client.on('ready', () => {
-    client.user.setStatus(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)]);
-    setInterval(() => {
-        client.user.setStatus(actvs[Math.floor(Math.random() * (actvs.length - 1) + 1)]);
-    }, 10000);
-});
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.login(process.env.BOT_TOKEN); 
+client.on('message', message => {
+    if (message.content === 'ping') {
+       message.reply('pong');
+       }
+});
+
+client.login(process.env.BOT_TOKEN);
